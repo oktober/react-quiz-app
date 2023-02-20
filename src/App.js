e2c88f7c70c7a1080c60938d4971b025e0f20842
oktober <5934988+oktober@users.noreply.github.com>
2023-02-20 15:50:14 -0800

    Change main app to functional component and remove extra components to simplify the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React, { Component } from "react"
-import Answer from "./components/Answer"
-import Question from "./components/Question"
+import { useState } from "react"
 import { questions } from "./questions"
 
-class App extends Component {
-    state = {
-        finished: false
-    }
+function App() {
+    const [ currentQuestion, setCurrentQuestion ] = useState(0);
 
-    render() {
-        let question = questions[0];
+    const question = questions[currentQuestion];
 
+    const answerInputs = Object.entries(question.answers).map( (value) => {
         return (
-            <main className="grid h-full place-items-center mt-8 border-2">
-                <h1 className="font-bold text-2xl mb-4">Name of Quiz</h1>
-                { this.state.finished ? <Answer /> : <Question question={question} /> }
-            </main>
-        );
-    };
+            <div key={value[0]} className="mt-2">
+                <input type="radio" value={value[0]} name="answer" id={value[0]} />
+                <label htmlFor={value[0]} className="ml-4">{value[1]}</label>
+            </div>
+        )
+    });
+
+    return (
+        <main className="grid h-full place-items-center mt-8 border-2">
+            <h1 className="font-bold text-2xl mb-4">Name of Quiz</h1>
+            <article>
+                <h2 className="font-semibold text-xl">Question #{question.id}</h2>
+
+                <legend className="text-l mt-2">{ question.question }</legend>
+
+                { answerInputs }
+
+                <button className="bg-slate-300 p-2 my-6 rounded-lg font-bold">Submit</button>
+            </article>
+        </main>
+    );
 }
 
 export default App
--- src/questions.js
+++ src/questions.js
@@ -8,8 +8,6 @@ export const questions = [
         "answer_b": "delete",
         "answer_c": "remove",
         "answer_d": "rmdir",
-        "answer_e": null,
-        "answer_f": null
       },
       "multiple_correct_answers": "false",
       "correct_answers": {
@@ -17,10 +15,31 @@ export const questions = [
         "answer_b_correct": "false",
         "answer_c_correct": "false",
         "answer_d_correct": "true",
-        "answer_e_correct": "false",
-        "answer_f_correct": "false"
       },
       "explanation": "rmdir deletes an empty directory",
+      "tip": null,
+      "tags": [],
+      "category": "linux",
+      "difficulty": "Easy"
+    },
+    {
+      "id": 2,
+      "question": "How to make a directory in Linux?",
+      "description": "delete folder",
+      "answers": {
+        "answer_a": "ls",
+        "answer_b": "mkdir",
+        "answer_c": "remove",
+        "answer_d": "rmdir",
+      },
+      "multiple_correct_answers": "false",
+      "correct_answers": {
+        "answer_a_correct": "false",
+        "answer_b_correct": "true",
+        "answer_c_correct": "false",
+        "answer_d_correct": "false",
+      },
+      "explanation": "mkdir creates an empty directory",
       "tip": null,
       "tags": [],
       "category": "linux",
